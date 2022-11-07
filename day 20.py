53217293b4c84b8ef8b543f5054cc9e3c33a5bf9
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-11-07 23:11:26 +0800

    Add files via upload

---
+++ day 20.py
@@ -0,0 +1,10 @@
+print("MENGHITUNG LUAS & KELILING PERSEGI PANJANG")
+
+panjang = int(input("\nMasukan Panjang: "))
+lebar = int(input("Masukan Lebar: "))
+
+luas = panjang*lebar
+keliling = 2 * (panjang+lebar)
+
+print("\nLuas Persegi Panjang \t\t:",luas)
+print("Keliling Persegi Panjang\t:",keliling)
